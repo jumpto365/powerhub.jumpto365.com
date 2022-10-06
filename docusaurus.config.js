fd53cce676ca7ae14790db96189cd224be115b60
Niels Gregers Johansen <niels@jumpto365.com>
2022-10-06 08:25:30 +0200

    bug: Corrected git project name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'jumpto365', // Usually your GitHub org/user name.
-  projectName: 'powerhub', // Usually your repo name.
+  projectName: 'powerhub.jumpto365.com', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -37,14 +37,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/jumpto365/powerhub/tree/main/',
+            'https://github.com/jumpto365/powerhub.jumpto365.com/tree/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-          'https://github.com/jumpto365/powerhub/tree/main/',
+          'https://github.com/jumpto365/powerhub.jumpto365.com/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -71,7 +71,7 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/jumpto365/powerhub',
+            href: 'https://github.com/jumpto365/powerhub.jumpto365.com',
             label: 'GitHub',
             position: 'right',
           },
@@ -115,7 +115,7 @@ const config = {
               },
               {
                 label: 'GitHub',
-                href: 'https://github.com/jumpto365/powerhub',
+                href: 'https://github.com/jumpto365/powerhub.jumpto365.com',
               },
             ],
           },
